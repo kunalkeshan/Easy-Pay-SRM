9b2b3f25e833203b74f99b36afcfe5c6867d0ed1
Kunal Keshan <kunalkeshan12@gmail.com>
2023-02-22 11:17:40 +0530

    chore: destop nav done

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
-	return <nav>Navbar</nav>;
+	const navOptions = [
+		{
+			page: 'Home',
+			url: '/',
+		},
+		{
+			page: 'Features',
+			url: '/#features',
+		},
+	];
+
+	return (
+		<nav className='w-full py-4 px-8 font-secondary'>
+			<div className='mx-auto flex w-full max-w-6xl items-center justify-between'>
+				<Link href='/' className='text-3xl font-semibold'>
+					easypay
+				</Link>
+				<ul className='flex gap-8 font-primary'>
+					{navOptions.map((option, index) => (
+						<li
+							key={index}
+							className='transition-all duration-300 hover:font-semibold'
+						>
+							<Link href={option.url}>{option.page}</Link>
+						</li>
+					))}
+				</ul>
+				<div className='flex items-center gap-4'>
+					<button className='btn'>Login</button>
+					<button className='btn border-none border-primary hover:!border hover:!bg-transparent hover:text-primary'>
+						Register now
+					</button>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
