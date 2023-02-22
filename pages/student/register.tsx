657407622e138eae49a5e857460134432c081fb5
Kunal Keshan <kunalkeshan12@gmail.com>
2023-02-22 12:37:10 +0530

    chore: student reg wip

--- pages/student/register.tsx
+++ pages/student/register.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Head from 'next/head';
+import PublicLayout from '@/layouts/PublicLayout';
 
 const StudentRegisterPage = () => {
-	return <div>StudentRegisterPage</div>;
+	return (
+		<>
+			<Head>
+				<title>Student Registration</title>
+			</Head>
+			<PublicLayout>
+				<section>
+					<div></div>
+				</section>
+			</PublicLayout>
+		</>
+	);
 };
 
 export default StudentRegisterPage;
